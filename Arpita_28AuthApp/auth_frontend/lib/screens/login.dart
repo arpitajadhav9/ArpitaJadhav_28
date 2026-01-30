1a6abd41d6ba0fba0573fb9bbb9e884081101937
Arpita Jadhav <142776140+arpitajadhav9@users.noreply.github.com>
2026-01-30 14:27:15 +0530

    login.dart updated

--- Arpita_28AuthApp/auth_frontend/lib/screens/login.dart
+++ Arpita_28AuthApp/auth_frontend/lib/screens/login.dart
@@ -2,106 +2,75 @@ import 'package:flutter/material.dart';
 import '../services/user_service.dart';
 import '../models/user.dart';
 import '../utils/custom_alert_box.dart';
-import 'package:jwt_decoder/jwt_decoder.dart';
+import 'package:jwt_decode/jwt_decode.dart';
 
 
 class LoginScreen extends StatefulWidget {
+
   LoginScreenState createState() => LoginScreenState();
 }
 
-class LoginScreenState extends State<LoginScreen>{
+class LoginScreenState extends State<LoginScreen> {
 
   final usernameController = TextEditingController();
-  final passwordController = TextEditingController(); 
+  final passwordController = TextEditingController();
 
   void handleLogin() async {
-
-    final Map<String, dynamic> response =
-        await UserService.login(
-          usernameController.text,
-          passwordController.text,
-        );
-
+    final response = await UserService.login(
+      usernameController.text,
+      passwordController.text,
+    );
     print(response);
 
-    if (response['message'] == 'Login Successful') {
-
-      String token = response['token'].toString();
-
-      Map<String, dynamic> decodedToken =
-          JwtDecoder.decode(token);
-
-      print('Decoded Token: $decodedToken');
+    if (response['message'] == 'Login successful') {
+      String token = response['token'];
+      Map<String, dynamic> decodedToken = Jwt.parseJwt(token);
+      print("Decoded Token: $decodedToken");
 
       User user = User(
-        id: decodedToken['userid'],
+        id: decodedToken['userId'],
         name: decodedToken['name'],
         username: decodedToken['username'],
         email: decodedToken['email'],
-        password: '',
+        password: "",
       );
 
-      CustomAlertBox.showAlert(
-        context,
-        title: 'Success',
-        message: response['message'].toString(),
-      );
+      CustomAlertBox.showSuccess(context,'Success',response['message'],);
 
-      Navigator.pushNamed(
-        context,
-        '/profile',
-        arguments: user,
-      );
-
+      Navigator.pushNamed(context,'/profile',arguments: user,);
     } else {
-
-      CustomAlertBox.showAlert(
-        context,
-        title: 'Error',
-        message: response['message'].toString(),
-      );
-
+      CustomAlertBox.showError(context,'Error',response['message'],);
     }
   }
-
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Login'),
+        title: Text("Login"),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(16),
-        child: Column(
-          children: [
-          
-            TextField(
-              controller: usernameController,
-              decoration: InputDecoration(
-                labelText: 'Username',
-                hintText: 'Enter your username',
-                icon: Icon(Icons.person),
-              ),
-            ),
-  
-            TextField(
-              controller: passwordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Password',
-                hintText: 'Enter your password',
-                icon: Icon(Icons.lock),
-              ),
-            ),
+      body: Column(
+        children: [
 
-            SizedBox(height: 20),
-
-            TextButton(
-              onPressed: handleLogin,
-              child: Text("Login"),
+          TextField(
+            controller: usernameController,
+            decoration: InputDecoration(
+              labelText: "Username",
+              hintText: "Enter your username",
+              icon: Icon(Icons.person)
             )
-
-          ],
-        ),
+          ),
+          TextField(
+            controller: passwordController,
+            decoration: InputDecoration(
+              labelText: "Password",
+              hintText: "Enter your password",
+              icon: Icon(Icons.password)
+            )
+          ),
+          TextButton(
+            child: Text("Login"),
+            onPressed: handleLogin,
+          )
+        ],
       ),
     );
   }
